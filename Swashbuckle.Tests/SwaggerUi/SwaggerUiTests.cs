62c9e6f5a3eacbea670c30a0046e0e368127e766
Benjamin Adams <ben@benadams.co>
2015-10-02 14:22:29 -0500

    removed test for removed files

--- Swashbuckle.Tests/SwaggerUi/SwaggerUiTests.cs
+++ Swashbuckle.Tests/SwaggerUi/SwaggerUiTests.cs
@@ -1,10 +1,10 @@
-﻿using System;
-using System.Net.Http;
-using System.Net;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using Swashbuckle.Application;
 using Swashbuckle.Dummy;
 using Swashbuckle.SwaggerUi;
+using System;
+using System.Net;
+using System.Net.Http;
 
 namespace Swashbuckle.Tests.SwaggerUi
 {
@@ -31,7 +31,7 @@ namespace Swashbuckle.Tests.SwaggerUi
             StringAssert.Contains("discoveryPaths: arrayFrom('swagger/docs/v1')", content);
             StringAssert.Contains("swagger-ui-container", content);
         }
-        
+
         [Test]
         public void It_exposes_config_to_inject_custom_stylesheets()
         {
@@ -41,7 +41,6 @@ namespace Swashbuckle.Tests.SwaggerUi
                     c.InjectStylesheet(assembly, "Swashbuckle.Dummy.SwaggerExtensions.testStyles1.css");
                     c.InjectStylesheet(assembly, "Swashbuckle.Dummy.SwaggerExtensions.testStyles2.css", "print");
                 });
-
 
             var content = GetContentAsString("http://tempuri.org/swagger/ui/index");
 
@@ -56,7 +55,7 @@ namespace Swashbuckle.Tests.SwaggerUi
             content = GetContentAsString("http://tempuri.org/swagger/ui/ext/Swashbuckle-Dummy-SwaggerExtensions-testStyles2-css");
             StringAssert.StartsWith("h2", content);
         }
-        
+
         [Test]
         public void It_exposes_config_for_swagger_ui_settings()
         {
@@ -71,7 +70,7 @@ namespace Swashbuckle.Tests.SwaggerUi
             StringAssert.Contains("docExpansion: 'full'", content);
             StringAssert.Contains("booleanValues: arrayFrom('1|0')", content);
         }
-        
+
         [Test]
         public void It_exposes_config_for_swagger_ui_outh2_settings()
         {
@@ -119,7 +118,7 @@ namespace Swashbuckle.Tests.SwaggerUi
             content = GetContentAsString("http://tempuri.org/swagger/ui/ext/Swashbuckle-Dummy-SwaggerExtensions-testScript2-js");
             StringAssert.StartsWith("var str2", content);
         }
-        
+
         [Test]
         public void It_exposes_config_to_serve_custom_assets()
         {
@@ -153,7 +152,7 @@ namespace Swashbuckle.Tests.SwaggerUi
 
             StringAssert.Contains("validatorUrl: stringOrNullFrom('null')", content);
         }
-        
+
         [Test]
         public void It_errors_on_asset_not_found_and_returns_status_not_found()
         {
@@ -162,13 +161,8 @@ namespace Swashbuckle.Tests.SwaggerUi
             Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode);
         }
 
-        [TestCase("http://tempuri.org/swagger/ui/images/logo_small-png",                   Result = "image/png")]
-        [TestCase("http://tempuri.org/swagger/ui/css/typography-css",                      Result = "text/css")]
-        [TestCase("http://tempuri.org/swagger/ui/fonts/droid-sans-v6-latin-regular-eot",   Result = "application/vnd.ms-fontobject")]
-        [TestCase("http://tempuri.org/swagger/ui/fonts/droid-sans-v6-latin-regular-woff",  Result = "application/font-woff")]
-        [TestCase("http://tempuri.org/swagger/ui/fonts/droid-sans-v6-latin-regular-woff2", Result = "application/font-woff2")]
-        [TestCase("http://tempuri.org/swagger/ui/fonts/droid-sans-v6-latin-regular-ttf",   Result = "application/font-sfnt")]
-        [TestCase("http://tempuri.org/swagger/ui/fonts/droid-sans-v6-latin-regular-svg",   Result = "image/svg+xml")]
+        [TestCase("http://tempuri.org/swagger/ui/images/logo_small-png", Result = "image/png")]
+        [TestCase("http://tempuri.org/swagger/ui/css/typography-css", Result = "text/css")]
         public string It_returns_correct_asset_mime_type(string resourceUri)
         {
             var response = Get(resourceUri);
