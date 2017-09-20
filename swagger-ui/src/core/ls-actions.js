85e68057107a06708ffe190cbe799636e968e55a
Benjamin Adams <armastevs@gmail.com>
2017-09-20 11:43:27 -0500

    No longer crashing when localstorage is full

--- swagger-ui/src/core/ls-actions.js
+++ swagger-ui/src/core/ls-actions.js
@@ -1,41 +1,52 @@
 //localstorage actions
 
-export function initHistory(){
+export function initHistory() {
   var hst = getXhrHistory()
-  if(!hst) {
-    window.localStorage.setItem('xhrHistory','[]')
+  if (!hst) {
+    window.localStorage.setItem('xhrHistory', '[]')
   }
 }
 
-export function addHistory(saveToHistory){
+export function addHistory(saveToHistory) {
   saveToHistory.dateAdded = new Date()
   var hst = getXhrHistory()
 
   //if(isItTheSameReq(saveToHistory, hst[0])) return 
 
-  hst.unshift(saveToHistory) //add history to start of array
-
-  if(hst.length > 150) {
+  if (hst.length > 100) {
     //delete the oldest request to free up space
-   hst.splice(hst.length-1, 1)
+    removeTwoHistoryItems(hst)
   }
 
-  window.localStorage.setItem('xhrHistory', JSON.stringify(hst))
+  hst.unshift(saveToHistory) //add history to start of array
+
+  try {
+    window.localStorage.setItem('xhrHistory', JSON.stringify(hst))
+  }
+  catch (e) {
+    removeTwoHistoryItems(hst)
+  }
+
 }
 
 export function getXhrHistory() {
-  var hst=window.localStorage.getItem('xhrHistory')
-  if(!hst) return []
+  var hst = window.localStorage.getItem('xhrHistory')
+  if (!hst) return []
   return JSON.parse(hst)
 }
 
 export function clearXhrHistory() {
-    window.localStorage.setItem('xhrHistory','[]')
+  window.localStorage.setItem('xhrHistory', '[]')
 }
 
 //ensure we dont submit duplicate requests
 function isItTheSameReq(saveToHistory, theLastReq) {
-  if(!theLastReq) return false       
-  if(saveToHistory.parameters === theLastReq.parameters && saveToHistory.routeId === theLastReq.routeId) return true
+  if (!theLastReq) return false
+  if (saveToHistory.parameters === theLastReq.parameters && saveToHistory.routeId === theLastReq.routeId) return true
   return false
+}
+
+
+function removeTwoHistoryItems(hst) {
+  hst.splice(hst.length - 1, 2)
 }
